3da3aaa148d3c2e76996c16f4c207b29abc801c8
grizzly <emilnissen2@gmail.com>
2024-11-20 16:29:04 +0100

    strict transport security

--- src/api/services/helper_services.go
+++ src/api/services/helper_services.go
@@ -7,12 +7,14 @@ import (
 
 func ResponseSuccess(w http.ResponseWriter, body map[string]interface{}, status int) {
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(body)
 }
 
 func ResponseError(w http.ResponseWriter, message string, status int) {
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(map[string]interface{}{
 		"status":  "error",
